889ec3d52895236dc8e24c03d37ce6c6dd7f399f
isstuev <isstuev@gmail.com>
2024-08-15 19:20:45 +0800

    build: vercel

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,8 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-import generateCspPolicy from 'nextjs/csp/generateCspPolicy';
 import * as middlewares from 'nextjs/middlewares/index';
 
-const cspPolicy = generateCspPolicy();
 
 export function middleware(req: NextRequest) {
   const isPageRequest = req.headers.get('accept')?.includes('text/html');
@@ -25,7 +23,6 @@ export function middleware(req: NextRequest) {
 
   const end = Date.now();
 
-  res.headers.append('Content-Security-Policy', cspPolicy);
   res.headers.append('Server-Timing', `middleware;dur=${ end - start }`);
   res.headers.append('Docker-ID', process.env.HOSTNAME || '');
 
